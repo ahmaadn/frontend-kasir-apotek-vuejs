2dc9962c63ea4af95e71f49cc5c455e1f840f32a
Ahmad Nur <ahmadnursahid12345@gmail.com>
2024-12-19 21:28:18 +0700

    feat: Add NProgress for loading indicators

--- src/middleware.js
+++ src/middleware.js
@@ -1,29 +1,37 @@
 import router from './router'
 import { useUserStore } from './stores/user'
 import { getToken } from './lib/auth'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 router.beforeEach(async (to, from, next) => {
+   NProgress.start()
    const hasToken = getToken()
 
    if (hasToken) {
       if (to.name == 'login') {
          next({ name: 'home' })
+         NProgress.done()
       } else {
          const user = useUserStore()
          try {
             // await user.getInfo()
             next()
+            NProgress.done()
          } catch (e) {
             user.logout()
             console.error(e)
             next({ name: 'login' })
+            NProgress.done()
          }
       }
    } else {
       if (to.meta.requiresAuth) {
          next(`/login?redirect=${to.path}`)
+         NProgress.done()
       } else {
          next()
+         NProgress.done()
       }
    }
 })
