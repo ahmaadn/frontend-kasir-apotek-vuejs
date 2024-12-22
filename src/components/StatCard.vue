8c8ba342f855dacb1048a0f4939bf2d54c66fa54
Ahmad Nur <ahmadnursahid12345@gmail.com>
2024-12-22 22:33:00 +0700

    feat: add StatCard component and integrate it into HomeView for displaying statistics

---
+++ src/components/StatCard.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Icon } from '@iconify/vue'
+
+const props = defineProps(['title', 'value', 'icon', 'desc', 'class'])
+</script>
+<template>
+   <div class="stat">
+      <div class="stat-figure" :class="props.class">
+         <Icon :icon="props.icon" class="inline-block h-8 w-8"></Icon>
+      </div>
+      <div class="stat-title">{{ props.title }}</div>
+      <div class="stat-value">{{ props.value }}</div>
+      <div v-if="props.desc" class="stat-desc">{{ props.desc }}</div>
+   </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,56 @@
+<script setup>
+import StatCard from '@/components/StatCard.vue'
+
+// import type { Header, Item, ClickRowArgument } from "vue3-easy-data-table";
+
+// omit
+
+const showRow = (item) => {
+   document.getElementById('row-clicked').innerHTML = JSON.stringify(item)
+}
+
+const headers = [
+   { text: 'PLAYER', value: 'player' },
+   { text: 'Action', value: 'action' },
+]
+
+const items = [
+   {
+      player: 'Stephen Curry',
+   },
+   {
+      player: 'Lebron James',
+   },
+]
+</script>
 <template>
-   <main>
-      <h1>Home</h1>
-      <p>Welcome to the Home page</p>
+   <main class="flex gap-4 flex-col">
+      <section class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+         <div class="stats shadow">
+            <StatCard
+               title="Jumlah Pegawai"
+               value="20"
+               icon="mdi:people-group-outline"
+               class="text-primary"
+            ></StatCard>
+         </div>
+         <div class="stats shadow">
+            <StatCard
+               title="Total Pendapatan"
+               value="25.6K"
+               icon="mdi:currency-usd"
+               class="text-success"
+               desc="Bulan ini"
+            ></StatCard>
+         </div>
+         <div class="stats shadow">
+            <StatCard
+               title="Total Obat"
+               value="25.6K"
+               icon="mdi:box-variant-closed"
+               class="text-warning"
+            ></StatCard>
+         </div>
+      </section>
    </main>
 </template>
