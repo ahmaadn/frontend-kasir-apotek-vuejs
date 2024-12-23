0920d6b974dc2deb22e22df352761fc1833006eb
Ahmad Nur <ahmadnursahid12345@gmail.com>
2024-12-23 20:28:20 +0700

    feat: add search and role filtering functionality to EmployeeView

--- src/views/EmployeeView.vue
+++ src/views/EmployeeView.vue
@@ -1,5 +1,4 @@
 <script setup>
-import InputForm from '@/components/Form/InputForm.vue'
 import SelectedCardForm from '@/components/Form/SelectedCardForm.vue'
 import { getUserList } from '@/lib/api/user'
 import { onMounted, ref } from 'vue'
@@ -8,6 +7,8 @@ import { toast } from 'vue-sonner'
 const loading = ref(true)
 const employeeList = ref([])
 const selected = ref(null)
+const search = ref('')
+const filterRole = ref([])
 
 const headers = [
    { text: 'Nama', value: 'fullname', sortable: true, width: 200 },
@@ -23,9 +24,12 @@ const budgeColor = {
    WRH: 'badge-accent bg-accent/20',
 }
 
-// const onSelected = ()
 const onSelected = (item) => {
    selected.value = item
+}
+
+const updateRole = (role) => {
+   filterRole.value = role
 }
 
 const loadEmployeeList = async () => {
@@ -42,6 +46,23 @@ const loadEmployeeList = async () => {
    loading.value = false
 }
 
+const filteredEmployeeList = () => {
+   let filtered = employeeList.value
+
+   if (search.value) {
+      filtered = filtered.filter((item) => {
+         return item.fullname.toLowerCase().includes(search.value.toLowerCase())
+      })
+   }
+
+   if (filterRole.value.length > 0) {
+      filtered = filtered.filter((item) => {
+         return filterRole.value.includes(item.role.rolename)
+      })
+   }
+   return filtered
+}
+
 onMounted(loadEmployeeList)
 </script>
 <template>
@@ -50,11 +71,21 @@ onMounted(loadEmployeeList)
          <button class="btn btn-success btn-sm text-white">Tambah Pegawai</button>
       </div>
       <div class="flex md:flex-row gap-4 justify-between flex-col">
-         <SelectedCardForm label="Filter Role" :items="['Admin', 'Pengelola Gudang', 'Kasir']" />
+         <SelectedCardForm
+            label="Filter Role"
+            :items="['Admin', 'Pengelola Gudang', 'Kasir']"
+            @update:model-value="updateRole"
+         />
+         <input
+            type="text"
+            placeholder="Cari nama Pegawai"
+            class="input input-sm input-bordered md:max-w-xs w-full grow rounded-md"
+            v-model="search"
+         />
       </div>
       <EasyDataTable
          :headers="headers"
-         :items="employeeList"
+         :items="filteredEmployeeList()"
          :loading="loading"
          :rows-items="[25, 50, 100]"
          buttons-pagination
