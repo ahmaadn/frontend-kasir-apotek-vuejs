5af1601bc202ad13b43543d60eff658175a71618
Ahmad Nur <ahmadnursahid12345@gmail.com>
2024-12-24 19:32:47 +0700

    feat: enhance InputForm component with icon support and dynamic class handling

--- src/components/Form/InputForm.vue
+++ src/components/Form/InputForm.vue
@@ -1,39 +1,36 @@
 <script setup>
 import { ErrorMessage, Field } from 'vee-validate'
+import { Icon } from '@iconify/vue'
+
 defineOptions({
    inheritAttrs: false,
 })
 
-const props = defineProps({
-   name: {
-      type: String,
-      require: true,
-   },
-   label: String,
-   showError: {
-      type: Boolean,
-      default: true,
-   },
-})
+const props = defineProps(['name', 'label', 'showError', 'class', 'iconLeft', 'iconRight'])
 </script>
 
 <template>
-   <div class="form-control">
+   <div class="form-control" :class="props.class">
       <div class="label text-lg font-semibold" v-if="props.label">
          <span class="label-text">{{ props.label }}</span>
-         <span class="label-text-alt text-error" v-if="props.showError">
+         <span class="label-text-alt text-error" :class="props.class?.error" v-if="props.showError">
             <ErrorMessage :name="props.name"></ErrorMessage>
          </span>
       </div>
-      <label class="group flex items-center gap-2" v-bind:for="props.name">
-         <slot name="icon-left" />
+      <label
+         class="group input input-bordered flex items-center gap-2 grow rounded-md input-sm h-10 leading-10 input-primary"
+         v-bind:for="props.name"
+      >
+         <Icon v-if="props.iconLeft" :icon="props.iconLeft" width="16" height="16"></Icon>
          <Field
-            class="mt-1 w-full grow rounded-md border-gray-200 bg-white text-sm"
+            class="grow"
+            autocomplete="off"
             v-bind="$attrs"
+            :class="props.class?.input"
             :name="props.name"
             :id="props.name"
          ></Field>
-         <slot name="icon-right" />
+         <Icon v-if="props.iconRight" :icon="props.iconRight" width="16" height="16"></Icon>
       </label>
    </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
    theme: {
       extend: {},
    },
-   plugins: [require('daisyui'), require('@tailwindcss/forms')],
+   plugins: [require('daisyui')],
    daisyui: {
       logs: false,
       themes: [
