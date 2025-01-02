7aff9f205ef14ab2b50ad874f579c8700549e3f2
Ahmad Nur <ahmadnursahid12345@gmail.com>
2025-01-02 20:52:49 +0700

    feat: add SidebarTooltip to SidebarHeader for improved navigation clarity

--- src/components/Sidebar/SidebarHeader.vue
+++ src/components/Sidebar/SidebarHeader.vue
@@ -1,4 +1,5 @@
 <script setup>
+import SidebarTooltip from './SidebarTooltip.vue'
 import { Icon } from '@iconify/vue'
 </script>
 
@@ -6,9 +7,13 @@ import { Icon } from '@iconify/vue'
    <div
       class="sidebar-header min-h-10 font-medium flex flex-shrink-0 w-full text-2xl text-start p-2"
    >
-      <router-link to="/" class="flex flex-row gap-x-2 items-center p-2">
+      <router-link
+         :to="{ name: 'Home' }"
+         class="flex flex-row gap-x-2 items-center p-2 sidebar-tooltip"
+      >
          <Icon icon="mdi:cart" class="h-10 w-10 min-h-10 min-w-10" />
          <h1 class="whitespace-nowrap">Kasir</h1>
+         <SidebarTooltip label="Home" class="left-14" />
       </router-link>
    </div>
 </template>
