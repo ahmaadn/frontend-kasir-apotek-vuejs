e00c14250c0307aa57737aa46f90290ebeffb13f
Ahmad Nur <ahmadnursahid12345@gmail.com>
2025-01-03 15:13:06 +0700

    fix: update header title in medicine index view for clarity

--- src/views/medicine/index-view.vue
+++ src/views/medicine/index-view.vue
@@ -4,7 +4,7 @@ import { MedicineTable, MedicineRestock } from '@/components/Medicine'
 <template>
    <main class="bg-base-100 shadow border rounded divide-y">
       <div class="py-4 px-6 flex flex-row justify-between items-center">
-         <h1 class="md:text-2xl text-lg font-medium">Daftar Pegawai Saat Ini</h1>
+         <h1 class="md:text-2xl text-lg font-medium">Daftar Obat Saat Ini</h1>
          <router-link :to="{ name: 'AddMedicine' }" class="btn btn-success btn-sm text-white">
             Tambah Obat
          </router-link>
